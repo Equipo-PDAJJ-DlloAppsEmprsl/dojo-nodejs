77dc52faf2dbda90e747eeb08310b0906fb854eb
Juan Gallo <juanfgallo@experimentality.co>
2020-09-25 21:24:58 -0500

    token added

--- dojo-nodejs/index.js
+++ dojo-nodejs/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
-const uri = require('./app/config/db.config.js')
+const uri = require('./app/config/db.config.js');
+const getToken = require('./app/services/token.service.js');
 
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 mongoose.set('useUnifiedTopology', true);
 mongoose.set('useNewUrlParser', true);
 
@@ -17,6 +18,8 @@ app.listen(PORT, () => {
  console.log(`Server is running on port ${PORT}.`);
 });
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
+app.get('/', async function (req, res) {
+    res.send('Espoti fai')
+    const token = await getToken();
+    console.log(token);
   })
