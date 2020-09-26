b11bde32459c73d3f0d6883c0abf1a86152b6dec
Belthgorr <44407888+Belthgorr@users.noreply.github.com>
2020-09-26 09:59:27 -0500

    Update token.service.js

--- dojo-nodejs/app/services/token.service.js
+++ dojo-nodejs/app/services/token.service.js
@@ -16,8 +16,8 @@ const getToken = async() => {
            "https://accounts.spotify.com/api/token",
            qs.stringify({
                grant_type: "client_credentials",
-               client_id: "77c097e19d8a464cb12d688421e0fb95",
-               client_secret: "1722299b96d4426895f9aea121715f93",
+               client_id: "",
+               client_secret: "",
            }),
            config
        )
